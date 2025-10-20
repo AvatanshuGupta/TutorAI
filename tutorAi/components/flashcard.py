12029198400f8daf1b73e0a0a527cded635b9323
AvatanshuGupta <avatanshugupta@gmail.com>
2025-10-21 01:07:15 +0530

    flashcard_model_changed

--- tutorAi/components/flashcard.py
+++ tutorAi/components/flashcard.py
@@ -9,7 +9,7 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
-llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash",api_key=os.getenv("GOOGLE_API_KEY_VIK"))
+llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash-lite",api_key=os.getenv("GOOGLE_API_KEY_VIK"))
 
 
 class FlashcardModel(BaseModel):
