d96a336f1a7245b0f88d1f82f90205b3cd0e9ac4
AvatanshuGupta <avatanshugupta@gmail.com>
2025-10-21 16:46:33 +0530

    host_final

--- tutorAi/settings.py
+++ tutorAi/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost',"https://tutorai-61hv.onrender.com/"]
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost',"tutorai-61hv.onrender.com"]
 
 
 # Application definition
